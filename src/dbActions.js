38ec2325bd74942b82d9a58a69d78f558a2b3084
Alex Anghel <alexisky@gmail.com>
2023-02-19 20:27:21 +0200

    undo base url

--- src/dbActions.js
+++ src/dbActions.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://tempus-notifico-api.onrender.com/';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export async function deleteFromDb(id) {
     await fetch(BASE_URL + "products/" + id, {
